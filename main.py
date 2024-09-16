d10239cb539aad3067a92bf994160a51d100896f
Colin <colinjw458@gmail.com>
2024-09-16 12:46:35 -0400

    start

---
+++ main.py
@@ -0,0 +1,6 @@
+import csp
+from csp import ts
+import operators
+
+if __name__ == "__main__":
+    print("Hello CSP!")
---
+++ operators.py
@@ -0,0 +1,12 @@
+import csp
+from csp import ts
+from datetime import datetime, timedelta
+
+# Returns a variable x time ago (wip)
+def delay(var, days):
+    return var
+
+# Returns the change in a variable x time ago (wip)
+def delta(var, days):
+    return var - delay(days)
+
