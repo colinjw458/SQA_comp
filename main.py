f3baabd3a84d1e73bb540b104a11bd21b62a0320
Colin <colinjw458@gmail.com>
2024-09-19 18:50:54 -0400

    CSP initial integration

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import csp
 from csp import ts
-import operators
+from datetime import datetime
 
-if __name__ == "__main__":
-    print("Hello CSP!")
+
+@csp.node
+def spread(bid: ts[float], ask: ts[float]) -> ts[float]:
+    if csp.valid(bid, ask):
+        return ask - bid
+
+
+@csp.graph
+def my_graph():
+    bid = csp.const(1.0)
+    ask = csp.const(2.0)
+    s = spread(bid, ask)
+
+    csp.print('spread', s)
+    csp.print('bid', bid)
+    csp.print('ask', ask)
+
+
+if __name__ == '__main__':
+    csp.run(my_graph, starttime=datetime.utcnow())
