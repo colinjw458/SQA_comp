24cf2adba1ac849b6f1f066cff930b36fc381ffd
Colin <colinjw458@gmail.com>
2024-09-23 15:17:25 -0400

    CSP almost works

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import csp
 from csp import ts
+import time
 from datetime import datetime, timedelta, timezone
 import numpy as np
 import pandas as pd
-import adapter
+import random
 
 sim_df = pd.read_csv("Data/preview.csv")
 
 
 @csp.node
-def portfolio_return(open: ts[float], prior_close: ts[float]) -> ts[float]:
+def portfolio_return(price: ts[float], prior_close: ts[float], delta: float) -> ts[float]:
     #Alarm acts as an event trigger
     with csp.alarms():
         event = csp.alarm(float)
@@ -17,25 +18,34 @@ def portfolio_return(open: ts[float], prior_close: ts[float]) -> ts[float]:
         s_count = 0
     #Schedules the alarm to trigger in x seconds
     with csp.start():
-        csp.schedule_alarm(event, timedelta(seconds=1), True)
+        print(delta)
+        csp.schedule_alarm(event, timedelta(seconds=delta), True)
 
     #Once triggered a new alarm must be set
     #At endtime the graph will stop running automatically so no need to worry about infinite loops
     if csp.ticked(event):
-        csp.schedule_alarm(event, timedelta(seconds=1), True)
+        csp.schedule_alarm(event, timedelta(seconds=delta), True)
         return (open - prior_close) / prior_close
 
-
-
-@csp.graph
+@csp.graph(memoize=False)
 def my_graph():
-    data = SimulaterEquityData(sim_df)
-    print(data)
-    port_return = portfolio_return(open, prior_close)
-    csp.print("data", port_return)
-    
+    prior_close = 0.0
+    for i, row in sim_df[["ts_recv","price"]].iterrows():
+        timeNow = datetime.fromisoformat(row["ts_recv"])
+        price = csp.const(row["price"])
+        if(i == 0):
+            prior_close = price
+            deltaTemp =  datetime.fromisoformat(sim_df["ts_recv"][i + 1]) - timeNow
+            time.sleep(deltaTemp.total_seconds())
 
-    csp.print('returns', port_return)
+        if(len(sim_df["ts_recv"]) == i + 1):
+            break
+        else:
+            deltaTemp =  datetime.fromisoformat(sim_df["ts_recv"][i + 1]) - timeNow
+            port_return = portfolio_return(price, prior_close, deltaTemp.total_seconds())
+            prior_close = price
+
+        csp.print('returns', port_return)
 
 #Change realtime to false for testing purposes
 if __name__ == '__main__':
