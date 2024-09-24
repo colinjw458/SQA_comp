d368ca6cd6dbcc9c450792f78d5cd36e5b618a7d
Colin <colinjw458@gmail.com>
2024-09-24 11:54:05 -0400

    Basic functionality

--- main.py
+++ main.py
@@ -4,49 +4,46 @@ import time
 from datetime import datetime, timedelta, timezone
 import numpy as np
 import pandas as pd
-import random
 
 sim_df = pd.read_csv("Data/preview.csv")
 
+# timedelta captures the difference of time for each row
+sim_df["ts_recv"] = pd.to_datetime(sim_df["ts_recv"])
+sim_df["timedelta"] = sim_df["ts_recv"].diff().shift(-1)
+sim_df["timedelta"].iloc[-1] = pd.Timedelta(seconds=0)
+print(sim_df)
+
 
 @csp.node
-def portfolio_return(price: ts[float], prior_close: ts[float], delta: float) -> ts[float]:
+def portfolio_return(df: pd.DataFrame) -> ts[float]:
     #Alarm acts as an event trigger
     with csp.alarms():
         event = csp.alarm(float)
+
     with csp.state():
-        s_count = 0
+        s = 0
+
     #Schedules the alarm to trigger in x seconds
     with csp.start():
-        print(delta)
+        delta = df["timedelta"].iloc[s].total_seconds()
         csp.schedule_alarm(event, timedelta(seconds=delta), True)
 
     #Once triggered a new alarm must be set
     #At endtime the graph will stop running automatically so no need to worry about infinite loops
     if csp.ticked(event):
+        s+=1
+        if(s + 1 == len(df)):
+            return (df["price"][s] - df["price"][s-1]) / df["price"][s-1]
+            
+        delta = df["timedelta"].iloc[s].total_seconds()
         csp.schedule_alarm(event, timedelta(seconds=delta), True)
-        return (open - prior_close) / prior_close
+        return (df["price"][s] - df["price"][s-1]) / df["price"][s-1]
 
 @csp.graph(memoize=False)
 def my_graph():
-    prior_close = 0.0
-    for i, row in sim_df[["ts_recv","price"]].iterrows():
-        timeNow = datetime.fromisoformat(row["ts_recv"])
-        price = csp.const(row["price"])
-        if(i == 0):
-            prior_close = price
-            deltaTemp =  datetime.fromisoformat(sim_df["ts_recv"][i + 1]) - timeNow
-            time.sleep(deltaTemp.total_seconds())
-
-        if(len(sim_df["ts_recv"]) == i + 1):
-            break
-        else:
-            deltaTemp =  datetime.fromisoformat(sim_df["ts_recv"][i + 1]) - timeNow
-            port_return = portfolio_return(price, prior_close, deltaTemp.total_seconds())
-            prior_close = price
-
-        csp.print('returns', port_return)
+    port_return = portfolio_return(sim_df[["timedelta","price"]])
+    csp.print('returns', port_return)
 
 #Change realtime to false for testing purposes
 if __name__ == '__main__':
-    csp.run(my_graph, starttime=datetime.now(timezone.utc), endtime=timedelta(seconds=10), realtime=True)
+    csp.run(my_graph, starttime=sim_df["ts_recv"].iloc[0], endtime=sim_df["ts_recv"].iloc[-1], realtime=True)
